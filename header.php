81d7b891d4a6cb87d5d48f8378f0dc065783c8a2
ac1dst0rm <sven.gasser@digitalnatives.ch>
2019-08-03 21:12:24 +0200

    New Design

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+    <title>Sven Gasser</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="Homepage of Sven Gasser">
+    <meta name="author" content="Sven Gasser">
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:700|Oswald&display=swap" rel="stylesheet"> 
+</head>
+
+<body>
+<div id="page">
+    <div id="header">
+        <div class="logo">
+        Sven Gasser
+        </div>
+    </div>
+    <ul class="nav global">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Portfolio</a></li>
+    </ul>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php    
+    require_once("header.php");
+?>
+<div id="container">
+    <div id="content">
+        <div>
+        
+        </div>
+    </div>
+</div>
+<?php
+    require_once("footer.php");
+?>
