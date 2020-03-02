8b415925309b133c600323abdf3444c2ded62608
st0rm <sven@HYPERBOOK.wir.banane.dir>
2020-03-02 21:17:10 +0100

    some code
    yea

--- footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-        <div class="footer-left">
-            <div>Sven Gasser | </div>
-        <div class="footer-right">
-            <div></div>
-    </div>
-    <script src="/assets/main.js"></script>
-</body>
-</html>
